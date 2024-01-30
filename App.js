1266821984233e891c91f36c16b2c5ea1fd9b9c4
Fulip <perfilipdaniel.sundstrom@elev.cybergymnasiet.se>
2024-01-30 19:11:37 +0100

    Start

---
+++ App.js
@@ -0,0 +1,29 @@
+// För själva servern
+const express = require("express");
+const app = express();
+
+// Kräv JSON filen
+// const data = require("./ordlista.json");
+
+// För att kunna skicka tillbaka html filer till en användare
+const path = require("path");
+
+// Behöver egentligen inte göra detta steg
+// Skapa en variabel som berättar vilken port vi lyssnar på
+const port = 3000;
+// För att våra html filer ska kunna komma åt en css fil t.ex.
+// Vi lägger då alla dessa filer i en mapp som heter public
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/Main.html"));
+});
+// Skapa dina paths här!
+app.get("/profile", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/Profile.html"));
+});
+
+// Starta servern och säg vilken port den ska lyssna på
+app.listen(port);
+// Gör en liten consol log så att användaren vet att allt gick bra
+console.log("Server is up and running at port " + port);
